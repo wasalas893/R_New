f06af18f723c3f26b078f6d1ecfea8a6b35221fc
wasalas <wasalas893@gmail.com>
2020-06-12 14:38:10 +0530

    create the backend routers

---
+++ server.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const connectDB=require('./config/db');
+const path=require('path');
+
+const app=express();
+
+//Connect Database
+connectDB();
+
+
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>res.send('ApI Running..'));
+
+
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
