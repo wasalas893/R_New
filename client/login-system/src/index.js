5336933218528885aae88d7376d3ff4486164ea8
wasalas <wasalas893@gmail.com>
2020-06-23 19:37:39 +0530

    remider page

--- client/login-system/src/index.js
+++ client/login-system/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {BrowserRouter as Router, Switch,Route,withRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Switch,Route,withRouter, BrowserRouter} from 'react-router-dom';
 
 import "semantic-ui-css/semantic.min.css";
 
--- routes/api/users.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-
-
-router.get('/',(req,res)=>res.send('User Post'));
-
-module.exports=router;
